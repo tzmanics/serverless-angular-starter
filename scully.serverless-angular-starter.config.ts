3f4df2ab1c945ba9a692f9c891dda5055be0f16a
Tara Z. Manicsic <tzmanics@gmail.com>
2021-04-11 00:49:49 -0400

    initial commit

---
+++ scully.serverless-angular-starter.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: "./src",
+  projectName: "serverless-angular-starter",
+  outDir: './dist/static',
+  routes: {
+  }
+};
